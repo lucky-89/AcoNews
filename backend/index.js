993a04071e2894b9dff01e189ee1273d51a36303
Gurudayal Maurya <115821196+lucky-89@users.noreply.github.com>
2024-09-14 12:34:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin:[""],
+    methods:["POST", "GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
